081fffa8d299a4eb7e79c0986c68f7e9b75dd71b
Nhan Phan <xeith1712@hotmail.com>
2018-08-27 12:19:11 -0700

    test

---
+++ config/routes.js
@@ -0,0 +1,5 @@
+const index_controller = require("../controllers/index.js");
+
+module.exports = function(app){
+  app.get('/', index_controller.renderMain);
+}
---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+const knex = require("../db/knex.js");
+
+module.exports = {
+  renderMain: (req, res)=>{
+    res.render('index', )
+  }
+}
