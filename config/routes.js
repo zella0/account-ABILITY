416948faf81c5fd71d6a4890bc439437c17552e4
Nhan Phan <xeith1712@hotmail.com>
2018-08-27 16:09:05 -0700

    login/register for user and owner

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,24 @@
 const index_controller = require("../controllers/index.js");
-
+const user_controller = require("../controllers/user.js");
 
 module.exports = function(app){
   app.get('/', index_controller.renderMain);
+
+  app.post('/userRegister', index_controller.userRegister);
+  app.post('/userLogin', index_controller.userLogin);
+
+  app.get('/ownerEntry', index_controller.renderOwnerEntry);
+  // app.post('/ownerEntry', index_controller.ownerRegister);
+  // app.post('/ownerEntry', index_controller.ownerLogin);
+
+  app.use(authMiddleWare);
+  app.get('/user/:id', user_controller.renderUser);
+}
+
+function authMiddleWare(req, res, next){
+  if(!req.session.user_id){
+    res.redirect('/entry');
+  }else{
+    next();
+  }
 }
--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,51 @@ const fs = require('fs');
 const API_KEY = fs.readFileSync(".apikey", 'utf8');
 
 module.exports = {
-  renderMain: (req, res)=>{
-    res.render('index', {
-      API_KEY: API_KEY
-    })
+  renderMain: (req, res) => {
+    if (req.session.user_id) {
+      res.redirect(`/user/${req.session.user_id}`)
+    } else {
+      res.render('index', {
+        API_KEY: API_KEY,
+      })
+    }
+  },
+  userRegister: (req, res) => {
+    knex('users')
+      .insert({
+        name: req.body.user_name,
+        email: req.body.user_email,
+        password: req.body.user_password
+      })
+      .then(() => {
+        res.redirect('/');
+      })
+  },
+  userLogin: (req, res) => {
+    knex('users')
+      .where('users.email', req.body.user_email)
+      .then((user) => {
+        if (user[0].password === req.body.user_password) {
+          req.session.user_id = user[0].id
+          res.redirect(`/user/${req.session.user_id}`)
+        } else {
+          res.redirect('/');
+        }
+      })
+  },
+  renderOwnerEntry: (req, res) => {
+    if (req.session.owner_id) {
+      res.redirect(`/owner/${req.session.owner_id}`)
+    } else {
+      res.render('owner_entry', {
+        API_KEY: API_KEY,
+      })
+    }
+  },
+  ownerRegister: (req, res) => {
+
+  },
+  ownerLogin: (req, res) => {
+
   }
 }
